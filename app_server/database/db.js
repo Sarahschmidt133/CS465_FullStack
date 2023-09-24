acd61894d5e0ef970a81a37de0405772a2debb84
Sarah Schmidt <sarahschmidt133@gmail.com>
2023-09-24 17:40:39 -0500

    module 4 - seed error

---
+++ app_server/database/db.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1'
+const dbURI = `mongodb://${host}/travlr`;
+// const dbURI = `mongodb://localhost:27017`;
+const readline = require('readline');
+
+// avoid 'current Server Discovery and Monitoring engine is deprecated'
+mongoose.set('useUnifiedTopology', true);
+
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        // useUnifiedTopology: true
+    }), 1000);
+}
+
+mongoose.connect.on('connected', () => {
+
+});
+
+mongoose.connect.on('error', err => {
+
+});
+
+mongoose.connect.on('disconnected', () => {
+
+});
+
+if (process.platform === 'win32') {
+};
+
+const gracefulShutdown = (msg, callback) => {
+
+};
+
+// For nodemon restart
+process.once('SIGUR2', () => {
+
+});
+
+// For app termination
+process.once('SIGINT', () => {
+
+});
+
+// For Heroku app terdmination
+process.once('SIGTERM', () => {
+
+});
+
+connect();
+
+// Brig the Mongoose schema
+require('./travlr');
+
